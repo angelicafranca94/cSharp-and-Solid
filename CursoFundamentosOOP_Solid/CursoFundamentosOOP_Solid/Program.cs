617337acf1293e1432ae6b1619f16dbe62ba9a17
Angélica França <gefranca94@gmail.com>
2022-03-30 15:48:17 -0300

    Métodos de Extensão

--- CursoFundamentosOOP_Solid/CursoFundamentosOOP_Solid/Program.cs
+++ CursoFundamentosOOP_Solid/CursoFundamentosOOP_Solid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CursoFundamentosOOP_Solid;
+using CursoFundamentosOOP_Solid.Métodos_de_Extensão;
 using CursoFundamentosOOP_Solid.Métodos_Estáticos;
 
 #region Encapsulamento e Modelo de Domínio
@@ -25,9 +26,22 @@ static ClienteAnemico ClienteAnemicoTeste()
 //Não há necessidade de se instanciar o objeto da classe para 
 //acessar o método quando esse método é static
 //Viola os paradgmas da POO
-Console.WriteLine("Soma = "+ Calculadora.Somar(2,4));
-Console.WriteLine("Soma = "+ Calculadora.Subtrair(4,2));
-Console.ReadLine();
+//Console.WriteLine("Soma = "+ Calculadora.Somar(2,4));
+//Console.WriteLine("Soma = "+ Calculadora.Subtrair(4,2));
 
 #endregion
 
+#region Métodos de Extensão
+
+var texto1 = "curso";
+var texto2 = "madalena";
+
+texto1 = texto1.CaixaAltaPrimeiraLetra();
+texto2 = texto2.CaixaAltaPrimeiraLetra();
+Console.WriteLine(texto1);
+Console.WriteLine(texto2);
+
+#endregion
+
+
+Console.ReadLine();
