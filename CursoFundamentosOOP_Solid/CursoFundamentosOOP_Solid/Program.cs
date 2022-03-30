1017e51e2851eaa92325fd361067d012a13ed648
Angélica França <gefranca94@gmail.com>
2022-03-30 15:32:51 -0300

    Métodos Estáticos

--- CursoFundamentosOOP_Solid/CursoFundamentosOOP_Solid/Program.cs
+++ CursoFundamentosOOP_Solid/CursoFundamentosOOP_Solid/Program.cs
@@ -1,13 +1,10 @@
 ﻿using CursoFundamentosOOP_Solid;
+using CursoFundamentosOOP_Solid.Métodos_Estáticos;
 
+#region Encapsulamento e Modelo de Domínio
 
-
-
-
-
-ClienteRico cliente = new ClienteRico(-1,"", null);
-
-Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
+//ClienteRico cliente = new ClienteRico(-1, "", null);
+//Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
 
 
 static ClienteAnemico ClienteAnemicoTeste()
@@ -20,3 +17,17 @@ static ClienteAnemico ClienteAnemicoTeste()
 
     return cliente;
 }
+
+#endregion
+
+#region Métodos Estáticos
+
+//Não há necessidade de se instanciar o objeto da classe para 
+//acessar o método quando esse método é static
+//Viola os paradgmas da POO
+Console.WriteLine("Soma = "+ Calculadora.Somar(2,4));
+Console.WriteLine("Soma = "+ Calculadora.Subtrair(4,2));
+Console.ReadLine();
+
+#endregion
+
